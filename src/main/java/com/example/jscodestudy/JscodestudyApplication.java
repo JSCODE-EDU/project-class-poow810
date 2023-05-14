fffd9a408613087cc1bbe71db85593f9202b210e
poow810 <poow810@naver.com>
2023-05-14 22:47:37 +0900

    init commit

---
+++ src/main/java/com/example/jscodestudy/JscodestudyApplication.java
@@ -0,0 +1,13 @@
+package com.example.jscodestudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JscodestudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JscodestudyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/jscodestudy/JscodestudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jscodestudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JscodestudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
